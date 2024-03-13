85dbcfd7306de51753558f82ba14dabe27fd2c30
David Lee <levansang.dna1@gmail.com>
2024-03-13 18:45:29 +0700

    Add project files.

---
+++ LoginandCreateacountCsharp/Login.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace LoginandCreateacountCsharp
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.txtTaikhoan = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.linkLabel1_quenmatkhau = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2_Dangky = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTaikhoan
+            // 
+            this.txtTaikhoan.Location = new System.Drawing.Point(135, 226);
+            this.txtTaikhoan.Multiline = true;
+            this.txtTaikhoan.Name = "txtTaikhoan";
+            this.txtTaikhoan.Size = new System.Drawing.Size(290, 30);
+            this.txtTaikhoan.TabIndex = 3;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(135, 299);
+            this.txtMatKhau.Multiline = true;
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.PasswordChar = '*';
+            this.txtMatKhau.Size = new System.Drawing.Size(290, 27);
+            this.txtMatKhau.TabIndex = 4;
+            // 
+            // linkLabel1_quenmatkhau
+            // 
+            this.linkLabel1_quenmatkhau.AutoSize = true;
+            this.linkLabel1_quenmatkhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1_quenmatkhau.Location = new System.Drawing.Point(132, 365);
+            this.linkLabel1_quenmatkhau.Name = "linkLabel1_quenmatkhau";
+            this.linkLabel1_quenmatkhau.Size = new System.Drawing.Size(115, 17);
+            this.linkLabel1_quenmatkhau.TabIndex = 5;
+            this.linkLabel1_quenmatkhau.TabStop = true;
+            this.linkLabel1_quenmatkhau.Text = "Quên Mật Khẩu?";
+            this.linkLabel1_quenmatkhau.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_quenmatkhau_LinkClicked);
+            // 
+            // linkLabel2_Dangky
+            // 
+            this.linkLabel2_Dangky.AutoSize = true;
+            this.linkLabel2_Dangky.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2_Dangky.Location = new System.Drawing.Point(365, 365);
+            this.linkLabel2_Dangky.Name = "linkLabel2_Dangky";
+            this.linkLabel2_Dangky.Size = new System.Drawing.Size(60, 17);
+            this.linkLabel2_Dangky.TabIndex = 6;
+            this.linkLabel2_Dangky.TabStop = true;
+            this.linkLabel2_Dangky.Text = "Đăng ký";
+            this.linkLabel2_Dangky.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_Dangky_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(242, 417);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Đăng nhập";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::LoginandCreateacountCsharp.Properties.Resources.mk;
+            this.pictureBox3.Location = new System.Drawing.Point(78, 285);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 41);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LoginandCreateacountCsharp.Properties.Resources.tk;
+            this.pictureBox2.Location = new System.Drawing.Point(79, 215);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 41);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = global::LoginandCreateacountCsharp.Properties.Resources.Admin;
+            this.pictureBox1.Location = new System.Drawing.Point(79, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(346, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 456);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel2_Dangky);
+            this.Controls.Add(this.linkLabel1_quenmatkhau);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTaikhoan);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txtTaikhoan;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.LinkLabel linkLabel1_quenmatkhau;
+        private System.Windows.Forms.LinkLabel linkLabel2_Dangky;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ LoginandCreateacountCsharp/Login.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginandCreateacountCsharp
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        Modify modify = new Modify();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string TK = txtTaikhoan.Text;
+            string MK = txtMatKhau.Text;
+            if (TK.Trim() == "") { MessageBox.Show("vui long nhập tên tài khoản ");  }
+            else if (MK.Trim() =="") { MessageBox.Show("vui long nhập Mật khẩu "); }
+            else
+            {
+               
+                string query = "Select * from TaiKhoan where TenTaiKhoan = '" + TK + "' and MatKhau ='" + MK + "'";
+                if (modify.taikhoans(query).Count>0)
+                {
+                    MessageBox.Show(" Đăng nhập thành công ","Thông báo",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    Home home = new Home();
+                    home.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show(" Tên tài khoản hoặc mật khẩu không chính xác ");
+
+                }
+            }
+
+        }
+
+        private void linkLabel1_quenmatkhau_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            QuenMatKhau quenMatKhau = new QuenMatKhau();
+            quenMatKhau.ShowDialog();
+
+        }
+
+        private void linkLabel2_Dangky_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Dangky dangky = new Dangky();
+            dangky.ShowDialog();
+
+        }
+    }
+}
